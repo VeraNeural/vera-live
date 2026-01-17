927a1796d1694e07cb987a8a30be312d89950eaf
VERA <vera@neural.space>
2026-01-16 21:35:38 -0800

    Revert Sanctuary transitions

--- src/app/sanctuary/layout.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import PageTransition from '@/components/sanctuary/PageTransition';
-
-export default function SanctuaryLayout({ children }: { children: React.ReactNode }) {
-  return <PageTransition>{children}</PageTransition>;
-}
